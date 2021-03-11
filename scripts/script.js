10b30fb70c6cd734f07665ebe4a962c6ba979205
Nikita Isaev <keller108@yandex.ru>
2021-03-11 14:54:27 +0500

    fix change

--- scripts/script.js
+++ scripts/script.js
@@ -1,23 +1,70 @@
-const popupEdit = document.querySelector('.popup-edit');
-const popupAdd = document.querySelector('.popup-add');
+// Переменные popup
+const popup = document.querySelector('.popup');
+const lightBoxImg = document.querySelector('.popup__image');
+const lightBoxTitle = document.querySelector('.popup__caption');
+const lightBox = document.querySelector('.popup_type_lightbox');
+const popupAdd = document.querySelector('.popup_type_card-add');
+const popupEdit = document.querySelector('.popup_type_profile');
+
+// Переменные форм
+const profileFormEdit = document.querySelector('.edit-form');
+const profileFormAdd = document.querySelector('.add-form');
 const inputName = document.querySelector('.form__input_el_place');
 const inputPictureLink = document.querySelector('.form__input_el_pic-link');
-const popupEditOpenBtn = document.querySelector('.profile__edit-button');
-const popupEditCloseBtn = document.querySelector('.popup-edit__close-btn');
-const popupAddOpenBtn = document.querySelector('.profile__add-button');
-const popupAddCloseBtn = document.querySelector('.popup-add__close-btn');
-const profileName = document.querySelector('.profile__name');
-const profileDescription = document.querySelector('.profile__description');
 const fieldName = document.querySelector('.form__input_el_name');
 const fieldDescr = document.querySelector('.form__input_el_descr');
-const submitEditForm = document.querySelector('.edit-form');
-const submitAddForm = document.querySelector('.add-form');
+
+// Переменные блока Profile
+const popupEditOpenBtn = document.querySelector('.profile__edit-button');
+const popupAddOpenBtn = document.querySelector('.profile__add-button');
+const profileName = document.querySelector('.profile__name');
+const profileDescription = document.querySelector('.profile__description');
+
+// Переменные template && card
 const templateCard = document.querySelector('.template');
 const cardsContainer = document.querySelector('.elements__cards');
-const lightBoxCloseBtn = document.querySelector('.lightbox__close-btn');
-const lightBox = document.querySelector('.lightbox');
-const lightBoxImg = document.querySelector('.lightbox__image');
-const lightBoxTitle = document.querySelector('.lightbox__caption');
+
+// Открытие попапов
+function openPopup(popup) {
+    popup.classList.add('popup_opened');
+}
+
+// Закрытие попапов
+function closePopup(popup) {
+    popup.classList.remove('popup_opened');
+};
+
+// Выбираю все кноки "закрыть попап"
+const popupCloseBtn = document.querySelectorAll('.popup__close-btn');
+
+//Добавляю слушателя для закрытия по клику на кнопку
+popupCloseBtn.forEach(function(item) {
+    item.addEventListener('click', evt => {
+        const popupToClose = evt.target.closest('.popup_opened');
+        closePopup(popupToClose);
+    });
+});
+
+// Слушатель для открытия попапа Edit
+popupEditOpenBtn.addEventListener('click', () => {
+    openPopup(popupEdit);
+    fieldName.value = profileName.textContent;
+    fieldDescr.value = profileDescription.textContent;
+});
+
+// Добавление данных из полей edit-profile в профиль
+profileFormEdit.addEventListener('submit', evt => {
+    evt.preventDefault();
+    profileName.textContent = fieldName.value;
+    profileDescription.textContent = fieldDescr.value;
+    closePopup(popupEdit);
+});
+
+popupAddOpenBtn.addEventListener('click', () => {
+    openPopup(popupAdd);
+    inputName.value = '';
+    inputPictureLink.value = '';
+});
 
 // Функция создания нового DOM узла
 function createCardDomNode(card) {
@@ -45,20 +92,15 @@ function renderClassList() {
     cardsContainer.prepend(...result);
 };
 
-function addCardFormListeners(evt) {
+profileFormAdd.addEventListener('submit', evt => {
     evt.preventDefault();
-
     const cardName = inputName.value;
     const cardImage = inputPictureLink.value;
-
     const addNewCard = createCardDomNode({ name: cardName, link: cardImage });
-
-    submitAddForm.reset();
-
     cardsContainer.prepend(addNewCard);
-
-    closePopup();
-};
+    closePopup(popupAdd);
+    submitAddForm.reset();
+});
 
 function deleteCardHandler(evt) {
     const currentCard = evt.target.closest('.elements__card');
@@ -69,8 +111,6 @@ function likeCardHandler(evt) {
     evt.target.classList.toggle('elements__like-btn_is_active');
 };
 
-renderClassList();
-
 function handleAddCardSubmit(cards) {
     const deleteBtn = cards.querySelector('.elements__delete-btn');
     deleteBtn.addEventListener('click', deleteCardHandler);
@@ -79,59 +119,13 @@ function handleAddCardSubmit(cards) {
     likeBtn.addEventListener('click', likeCardHandler);
 
     const openLightBoxBtn = cards.querySelector('.elements__card-image');
-
     openLightBoxBtn.addEventListener('click', function(evt) {
-        lightBox.classList.add('lightbox_opened');
+        lightBox.classList.add('popup_opened');
         const target = evt.target;
         lightBoxImg.src = target.src;
         lightBoxImg.alt = target.closest('.elements__card').querySelector('.elements__place-name').textContent;
         lightBoxTitle.textContent = target.closest('.elements__card').querySelector('.elements__place-name').textContent;
     });
-
 };
 
-// Функция открытия попапа Edit
-function openEditPopup() {
-    popupEdit.classList.add('popup-edit_opened');
-    fieldName.value = profileName.textContent;
-    fieldDescr.value = profileDescription.textContent;
-};
-
-// Функция открытия попапа Add
-function openAddPopup() {
-    popupAdd.classList.add('popup-add_opened');
-};
-
-function openLightbox() {
-    lightBox.classList.add('lightbox_opened');
-};
-
-// Функция добавления данных из полей edit-profile в профиль
-function formSubmit(evt) {
-    evt.preventDefault();
-    profileName.textContent = fieldName.value;
-    profileDescription.textContent = fieldDescr.value;
-    closePopup();
-};
-
-// Функция закрытия попапов
-function closePopup() {
-    //Закрываем popupEdit
-    popupEdit.classList.remove('popup-edit_opened');
-
-    //Закрываем popupAdd
-    popupAdd.classList.remove('popup-add_opened');
-
-    lightBox.classList.remove('lightbox_opened');
-};
-
-popupEditOpenBtn.addEventListener('click', openEditPopup);
-popupAddOpenBtn.addEventListener('click', openAddPopup);
-
-submitEditForm.addEventListener('submit', formSubmit);
-submitAddForm.addEventListener('submit', addCardFormListeners);
-
-
-popupEditCloseBtn.addEventListener('click', closePopup);
-popupAddCloseBtn.addEventListener('click', closePopup);
-lightBoxCloseBtn.addEventListener('click', closePopup);
+renderClassList();
