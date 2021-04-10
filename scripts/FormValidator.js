be6eeb7a62bccb0d09d657420b963b091b17968b
Nikita Isaev <keller108@yandex.ru>
2021-04-10 13:57:35 +0500

    ref/ FormValidator works

--- scripts/FormValidator.js
+++ scripts/FormValidator.js
@@ -23,38 +23,11 @@ export default class FormValidator {
         btn.classList.remove(object.inactiveButtonClass);
     };
 
-    // Ф-йия переключения состояния кнопки
-    _toggleButtonState(inputList, buttonElement, object) {
-        if (hasInvalidInput(inputList, object) || allInputsEmpty(inputList, object)) {
-            disableSubmitBtn(buttonElement, object);
-            buttonElement.setAttribute('disabled', true);
-        } else {
-            enableSubmitBtn(buttonElement, object);
-            buttonElement.removeAttribute('disabled');
-        }
-    };
-
-    // Покажем ошибку
-    _showInputError(formElement, inputElement, errorMessage, object) {
-        const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
-        inputElement.classList.add(object.inputErrorClass);
-        errorElement.textContent = errorMessage;
-        errorElement.classList.add(object.errorClass);
-    };
-
-    // Спрячем ошибку
-    _hideInputError(formElement, inputElement, object) {
-        const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
-        inputElement.classList.remove(object.inputErrorClass);
-        errorElement.classList.remove(object.errorClass);
-        errorElement.textContent = '';
-    };
-
     _checkInput(formElement, inputElement, object) {
         if (inputElement.validity.valid) {
-            hideInputError(formElement, inputElement, object);
+            this.hideInputError(formElement, inputElement, object);
         } else {
-            showInputError(formElement, inputElement, inputElement.validationMessage, object);
+            this.showInputError(formElement, inputElement, inputElement.validationMessage, object);
         }
     };
 
@@ -63,26 +36,51 @@ export default class FormValidator {
         const inputList = Array.from(formElement.querySelectorAll(object.inputSelector));
         const buttonElement = formElement.querySelector(object.submitButtonSelector);
 
-        inputList.forEach(inputElement => {
+        inputList.forEach((inputElement) => {
             inputElement.addEventListener('input', () => {
-                checkInput(formElement, inputElement, object);
-                toggleButtonState(inputList, buttonElement, object);
+                this._checkInput(formElement, inputElement, object);
+                this.toggleButtonState(inputList, buttonElement, object);
             });
         });
-        toggleButtonState(inputList, buttonElement, object);
+        // this.toggleButtonState(inputList, buttonElement, object);
+    };
+
+    // Ф-йия переключения состояния кнопки
+    toggleButtonState(inputList, buttonElement, object) {
+        if (this._hasInvalidInput(inputList, object) || this._allInputsEmpty(inputList, object)) {
+            this._disableSubmitBtn(buttonElement, object);
+            buttonElement.setAttribute('disabled', true);
+        } else {
+            this._enableSubmitBtn(buttonElement, object);
+            buttonElement.removeAttribute('disabled');
+        }
+    };
+
+    // Покажем ошибку
+    showInputError(formElement, inputElement, errorMessage, object) {
+        const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+        inputElement.classList.add(object.inputErrorClass);
+        errorElement.textContent = errorMessage;
+        errorElement.classList.add(object.errorClass);
+    };
+
+    // Спрячем ошибку
+    hideInputError(formElement, inputElement, object) {
+        const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+        inputElement.classList.remove(object.inputErrorClass);
+        errorElement.classList.remove(object.errorClass);
+        errorElement.textContent = '';
     };
 
     // Опишем функцию включения валидации
-    _enableValidation(object) {
+    enableValidation(object) {
         const formList = Array.from(document.querySelectorAll(object.formSelector));
 
         formList.forEach((formElement) => {
             formElement.addEventListener('submit', (evt) => {
                 evt.preventDefault();
             });
-            setInputListeners(formElement, object);
+            this._setInputListeners(formElement, object);
         });
     };
-
-    // Вызовем функцию
-    enableValidation(validationObject);
+}
--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,5 @@
 import Card from './Card.js';
-// import FormValidator from './FormValidator.js/index.js';
+import FormValidator from './FormValidator.js';
 
 initialCards.forEach((item) => {
     cardsContainer.prepend(new Card(item.name, item.link).generateCard());
@@ -70,64 +70,11 @@ popupAddOpenBtn.addEventListener('click', () => {
 // Добавление карточки через форму
 profileFormAdd.addEventListener('submit', evt => {
     evt.preventDefault();
-    const cardName = inputName.value;
-    const cardImage = inputPictureLink.value;
-    const addNewCard = createCardDomNode({ name: cardName, link: cardImage });
-    cardsContainer.prepend(addNewCard);
+    cardsContainer.prepend(new Card(inputName.value, inputPictureLink.value).generateCard());
     closePopup(popupAdd);
     profileFormAdd.reset();
 });
 
-// // Функция создания нового DOM узла
-// function createCardDomNode(card) {
-
-//     const newItem = templateCard.content.querySelector('.elements__card').cloneNode(true);
-//     const domCardImg = newItem.querySelector('.elements__place-name');
-
-//     domCardImg.textContent = card.name;
-//     domCardImg.alt = card.name;
-//     newItem.querySelector('.elements__card-image').src = card.link;
-
-//     handleAddCardSubmit(newItem);
-//     return newItem;
-
-// };
-
-// // Функция рендера карточек на странице
-// function renderClassList() {
-//     const result = initialCards.map(function(item) {
-//         const newCard = createCardDomNode(item);
-//         return newCard;
-//     });
-
-//     //Отрисуем
-//     cardsContainer.prepend(...result);
-// };
-
-// function deleteCardHandler(evt) {
-//     const currentCard = evt.target.closest('.elements__card');
-//     currentCard.remove();
-// };
-
-// function likeCardHandler(evt) {
-//     evt.target.classList.toggle('elements__like-btn_is_active');
-// };
-
-// function handleAddCardSubmit(cards) {
-//     const deleteBtn = cards.querySelector('.elements__delete-btn');
-//     deleteBtn.addEventListener('click', deleteCardHandler);
-
-//     const likeBtn = cards.querySelector('.elements__like-btn');
-//     likeBtn.addEventListener('click', likeCardHandler);
-
-//     const openLightBoxBtn = cards.querySelector('.elements__card-image');
-//     openLightBoxBtn.addEventListener('click', function(evt) {
-//         openPopup(lightBox);
-//         const target = evt.target;
-//         lightBoxImg.src = target.src;
-//         lightBoxImg.alt = target.closest('.elements__card').querySelector('.elements__place-name').textContent;
-//         lightBoxTitle.textContent = target.closest('.elements__card').querySelector('.elements__place-name').textContent;
-//     });
-// };
-
-// renderClassList();
+// Запуск валидации
+const formValidation = new FormValidator(validationObject);
+formValidation.enableValidation(validationObject);
