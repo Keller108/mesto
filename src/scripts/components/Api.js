73ff938f8822ad08fc3e4041d2e29e78556a790d
Nikita Isaev <keller108@yandex.ru>
2021-05-08 09:23:06 +0500

    js/ profile edit done

--- src/scripts/components/Api.js
+++ src/scripts/components/Api.js
@@ -24,9 +24,9 @@ export default class Api {
     // Отправка информации профиля
     sendInfo(inputsValue) {
         const newData = {
+            method: 'PATCH',
             ...this._config,
             body: JSON.stringify(inputsValue),
-            method: 'PATCH',
         }
         return fetch('https://mesto.nomoreparties.co/v1/cohort-23/users/me', newData)
             .then(res => res.ok ?
